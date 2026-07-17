6475f485fd80c4012352d7923097268502024820
Jonnathan23 <jsaquicela26@gmail.com>
2026-07-16 21:25:03 -0500

    refactor(shared): implement singleton generators for adapters

--- src/shared/core/helpers/generators.helper.ts
+++ src/shared/core/helpers/generators.helper.ts
@@ -1,5 +1,6 @@
 import { adapterUuidGenerator } from "src/shared/core/adapters/uuid/di/uuid.dependencies";
+import type { Uuid } from "src/shared/core/types/uuid.type";
 
-import type { UuidStrategy } from "src/shared/core/adapters/uuid/domain/interface/uuid-strategy.interface";
-
-export const uuidHelper: UuidStrategy = adapterUuidGenerator;
+export const uuidHelper = (): Uuid => {
+    return adapterUuidGenerator.generateUuid();
+};
