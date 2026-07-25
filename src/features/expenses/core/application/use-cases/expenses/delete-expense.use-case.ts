addd38c653f547407eb1c98e0949bf747d63287f
Jonnathan23 <jsaquicela26@gmail.com>
2026-07-24 22:40:35 -0500

    fix(trans): delete files about expenses in applicaction and domain

--- src/features/expenses/core/application/use-cases/expenses/delete-expense.use-case.ts
+++
@@ -1,9 +0,0 @@
-import { ExpenseRepository } from "src/features/expenses/core/domain/repositories/expense.repository";
-
-export class DeleteExpenseUseCase {
-    public constructor(private readonly expenseRepository: ExpenseRepository) {}
-
-    public execute(id: string): Promise<void> {
-        return this.expenseRepository.deleteExpense(id);
-    }
-}
